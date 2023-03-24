e9ab76dadbea5d1b2366d7d5ae7c181d853a9508
vollcheck <jaw@juxt.pro>
2023-03-24 17:42:08 +0100

    organized fetching ns, kixi.stats installed + changed videos

--- README.md
+++ README.md
@@ -3,14 +3,17 @@
 sentiment analysis on contrast comments on two youtube videos -
 one containing so-called "freak fight" (poor mans box)
 and second with "good vibes" music
+due to nlp model/corpora I would use english online resources
 
 ## resources
 
 ### negative(?) sentiment video
-https://www.youtube.com/watch?v=plNJOq9pMnY&ab_channel=FAMEMMA
+;; https://www.youtube.com/watch?v=plNJOq9pMnY&ab_channel=FAMEMMA
+https://www.youtube.com/watch?v=YwNj8ZycZMI&ab_channel=SlapFightingChampionship
 ### positive(?) sentiment video
-https://www.youtube.com/watch?v=2DiP0mMeaT8&ab_channel=DawidPodsiad%C5%82o
-;; https://www.youtube.com/watch?v=1Pg1RguhqxY&ab_channel=ememusicVEVO
+;; https://www.youtube.com/watch?v=2DiP0mMeaT8&ab_channel=DawidPodsiad%C5%82o
+https://www.youtube.com/watch?v=1Pg1RguhqxY&ab_channel=ememusicVEVO
+
 
 ## usage
 
@@ -22,3 +25,11 @@ in order to run it locally you need to provide environmental variable called
 - you should value the comments by the number of reactions to it - relevance
 - use conn manager https://cljdoc.org/d/clj-http/clj-http/3.12.3/api/clj-http.conn-mgr#make-reusable-conn-manager
 - cache results - save youtube api quota
+
+## references
+- libpython-clj + spacy interop example:
+  https://github.com/gigasquid/libpython-clj-examples/blob/master/src/gigasquid/spacy.clj
+
+- spacy - sentiment analysis using textblob
+  https://spacy.io/universe/project/spacy-textblob
+  https://textblob.readthedocs.io/en/dev/quickstart.html#sentiment-analysis
--- src/sentymenty/core.clj
+++ src/sentymenty/core.clj
@@ -1,78 +1,43 @@
 (ns sentymenty.core
   (:require
    [clojure.string :as str]
-   [clojure.java.io :as io]
-   [clj-http.client :as http-client]))
+   [libpython-clj2.require :refer [require-python]]
+   [libpython-clj2.python :as py :refer [py. py.. py.-]]
+   [kixi.stats.core :refer [mean]]))
 
-(def API_KEY
-  (or (str/trim (slurp ".env"))
-      (System/getenv "YOUTUBE_API_KEY")))
+(require-python '[spacy :as spacy]
+                '[spacytextblob.spacytextblob.SpacyTextBlob :as SpacyTextBlob])
 
-(def comments-api-url
-  (str "https://www.googleapis.com/youtube/v3/commentThreads"
-       ;; "?part=snippet%2Creplies"
-       "?part=snippet,replies"
-       "&maxResults=100"
-       "&key=" API_KEY
-       "&videoId=%s"
-       "&pageToken=%s"))
+(def nlp
+  (let [nlp (spacy/load "en_core_web_sm")]
+    (py. nlp add_pipe "spacytextblob")
+    nlp))
 
-(def negative-video-id "plNJOq9pMnY")
-(def positive-video-id "2DiP0mMeaT8")
+(defn text->polarity [nlp' text]
+  (py.. (nlp' text)
+        -_ -blob -polarity)) ;; like nlp._.blob.polarity in Python
 
-(defn api-call [video-id page-token]
-  (let [url (format comments-api-url video-id page-token)]
-    ;; TODO use conn manager
-    (http-client/get url {:accept :json
-                          :as :json
-                          :cache true})))
+(defn load-and-compute [nlp' path]
+  (->> (slurp path)
+       str/split-lines
+       (map (partial text->polarity nlp'))))
 
-(defn extract-comments
-  "Giving the response for max results (100) extract the comment strings"
-  [response]
-  (->> (get-in response [:body :items])
-       (reduce
-        (fn [acc entity]
-          (let [entity (:snippet entity)
-                top-level (get-in entity [:topLevelComment :snippet :textOriginal])
-                replies (when (pos? (:totalReplyCount entity))
-                          (map
-                           #(get-in % [:snippet :textOriginal])
-                           (get-in entity [:replies :comments])))]
-            (if replies
-              (conj acc top-level replies)
-              (conj acc top-level))))
-        [])))
-
-(defn fetch-comments
-  "General abstraction for handling with paged API responses"
-  [video-id]
-  (into []
-        conj
-        (iteration (partial api-call video-id)
-                   {:somef #(= 200 (:status %))
-                    :vf extract-comments
-                    :kf #(get-in % [:body :nextPageToken])
-                    :initk ""})))
-
-(defn dump-sentences [filename content]
-  (with-open [w (io/writer filename)]
-    (binding [*out* w]
-      (doseq [line content]
-        (.write w (str line "\n"))))))
-
-(defn clean-comments [comments]
-  (->> comments
-       (flatten)
-       (filter string?)))
-
-(def negative-video-comments
-    (clean-comments (fetch-comments negative-video-id)))
-
-(def positive-video-comments
-  (clean-comments (fetch-comments positive-video-id)))
+(defn mean [coll]
+  (/ (reduce + 0 coll) (count coll)))
 
 (comment
-  (dump-sentences "resources/negative-sentiment.txt" negative-video-comments)
-  (dump-sentences "resources/positive-sentiment.txt" positive-video-comments)
+  "resources/positive-sentiment.txt"
+  "resources/negative-sentiment.txt"
+
+  (def pos-coll (load-and-compute nlp
+                                  "resources/positive-sentiment.txt"))
+
+  (def neg-coll (load-and-compute nlp
+                                  "resources/negative-sentiment.txt"))
+
+  (transduce identity mean pos-coll)
+   ;; => 0.14902176846455445
+
+  (transduce identity mean neg-coll)
+   ;; => -0.013954982722558225
   )
